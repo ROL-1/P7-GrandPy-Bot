9e44b9e00e9defce07913766c8582ef1334ead80
ROL-1 <loicromero1@gmail.com>
2020-10-25 11:18:39 +0100

    fix(js) fix ul selection in empty_zone & paste 'Scrollspy smooth' in jquery.js

--- grandPyApp/static/js/jquery.js
+++ grandPyApp/static/js/jquery.js
@@ -1,14 +1,29 @@
+// Fill Empty_zone with input text
 $(function() {
 
-    var $list, $question;
-    $list = $('ul');
-    $question = $('#question');
-  
-    $question.on('submit', function(e) {
-      e.preventDefault();
-      var text = $('input:text').val();
-      $list.append('<li>' + text + '</li>');
-      $('input:text').val('');
-    }); 
+  var $list, $question;
+  $list = $('#dialog ul');
+  $question = $('#question');
 
+  $question.on('submit', function(e) {
+    e.preventDefault();
+    var text = $('input:text').val();
+    $list.append('<li>' + text + '</li>');
+    $('input:text').val('');
+  }); 
+
+});
+
+
+// Scrollspy smooth
+$(function () {
+  $('header a').on('click', function(e) {
+  e.preventDefault();
+  var hash = this.hash;
+  $('html, body').animate({
+    scrollTop: $(this.hash).offset().top
+  }, 1000, function(){
+    window.location.hash = hash;
   });
+  });
+});
