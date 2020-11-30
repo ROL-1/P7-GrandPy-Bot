6d8681368e04d0ce1a6f5a06b3f0d919e044d2fc
ROL-1 <loicromero1@gmail.com>
2020-11-30 23:33:07 +0100

    fix(settings.py) trailling space in api key

--- grandPyApp/settings.py
+++ grandPyApp/settings.py
@@ -4,7 +4,8 @@ import os
 
 # SECURITY WARNING: keep the secret key used in production secret!
 MAPBOX_API_KEY = os.getenv(
-    "MAPBOX_API_KEY ", #"ADD_YOUR_MAPBOX_API_KEY_HERE"
+    "MAPBOX_API_KEY",
+    #"ADD_YOUR_MAPBOX_API_KEY_HERE"
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
